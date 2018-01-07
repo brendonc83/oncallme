fd1b2263e2d63d5dfe3fc12b036057c5ffb3b529
Brendon <bl.cleary@gmail.com>
2018-01-08 09:37:07 +1000

    initial requirements and db migrations

--- on_call_me/models.py
+++ on_call_me/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class TeamMember(models.Model):
+    u_name = models.CharField(max_length=8, primary_key=True)
+    full_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=20)
+
+
+class OnCallPeriod(models.Model):
+
+    team_member = models.ForeignKey(TeamMember, on_delete=models.CASCADE)
+    week_ending = models.DateField('week ending')
+    start_date = models.DateField('start date')
+    end_date = models.DateField('end date')
+    hours = models.IntegerField(default=0)
