08d6adb136868e80399596b5ea3b6123cb1c4558
Brendon <bl.cleary@gmail.com>
2018-01-08 09:37:07 +1000

    initial requirements and db migrations

--- oncallme/settings.py
+++ oncallme/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Database environment variables
+PG_USER = os.environ.get('POSTGRES_USER')
+PG_PWD = os.environ.get('POSTGRES_PWD')
 
 # Application definition
 
@@ -72,13 +75,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'oncallme.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'oncallmedb',
+        'USER': PG_USER,
+        'PASSWORD': PG_PWD,
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
