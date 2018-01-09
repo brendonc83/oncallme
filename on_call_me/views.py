d092fd6d028b387777a79785c033f8615d6dc482
Brendon <bl.cleary@gmail.com>
2018-01-09 21:00:53 +1000

    Added template and some error handling for the user_detail view

--- on_call_me/views.py
+++ on_call_me/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from on_call_me import models
+from django.shortcuts import get_object_or_404, render
+from .models import TeamMember
 
 
 def index(request):
     return HttpResponse("Welcome to index")
 
+"""
+# Provide the username and get user details
+def user_detail(request, u_id):
+    try:
+        user_info = (TeamMember.objects.get(u_name=u_id))
+        context = {'user_info': user_info}
+    except TeamMember.DoesNotExist:
+        raise Http404("TeamMember does not exist")
+    return render(request, 'on_call_me/index.html', context)
+"""
+
 
 # Provide the username and get user details
 def user_detail(request, u_id):
+    print(u_id)
+    user_info = get_object_or_404(TeamMember, u_name=u_id)
+    context = {'user_info': user_info}
 
-    user_info = models.TeamMember.objects.get(u_name=u_id)
-
-    return HttpResponse("This is the user info %s %s" % (user_info.full_name, user_info.role))
-
+    return render(request, 'on_call_me/index.html', context)
 
