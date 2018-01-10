29b7a368a7d572d8702cdc8d605ea2cc58198c1e
Brendon <bl.cleary@gmail.com>
2018-01-10 12:53:21 +1000

    update .gitignore to ignore .pyc files

--- on_call_me/views.py
+++ on_call_me/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 from .models import TeamMember
 
 
@@ -18,11 +21,17 @@ def user_detail(request, u_id):
 """
 
 
+"""
 # Provide the username and get user details
 def user_detail(request, u_id):
     print(u_id)
     user_info = get_object_or_404(TeamMember, u_name=u_id)
     context = {'user_info': user_info}
-
     return render(request, 'on_call_me/index.html', context)
+"""
 
+
+class TeamMemberDetailView(generic.DetailView):
+
+    model = TeamMember
+    template_name = 'on_call_me/index.html'
