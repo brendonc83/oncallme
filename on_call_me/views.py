a19fe792bbaa9d395d169e43dba7d98b02aa9de6
Brendon <bl.cleary@gmail.com>
2018-01-19 22:07:41 +1000

    basic login and logout

--- on_call_me/views.py
+++ on_call_me/views.py
@@ -1,19 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.template import Context, loader
-from django.views import generic
-from .models import User
+from django.template import loader
+from django.template import RequestContext
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
-def login(request):
-    template = loader.get_template('on_call_me/login.html')
-    return HttpResponse(template.render())
-
-
+@login_required()
 def index(request):
-    return HttpResponse("Welcome to index")
+
+    return render(request, 'on_call_me/index.html', {
+        'username': 'user.username',
+        'first_name': 'user.first_name',
+        'last_name': 'user.lastname',
+        'email': 'user.email',
+    })
 
 
 """
@@ -34,4 +33,19 @@ def user_detail(request, u_id):
     user_info = get_object_or_404(TeamMember, u_name=u_id)
     context = {'user_info': user_info}
     return render(request, 'on_call_me/index.html', context)
+"""
+
+
+"""
+def view_1(request):
+    # ...
+    t = loader.get_template('template1.html')
+    c = Context({
+        'app': 'My app',
+        'user': request.user,
+        'ip_address': request.META['REMOTE_ADDR'],
+        'message': 'I am view 1.'
+    })
+    return t.render(c)
+
 """
