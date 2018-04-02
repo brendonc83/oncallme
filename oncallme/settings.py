cc4b7ef2e7baa453808f405dd64ac880564df8ba
Brendon <bl.cleary@gmail.com>
2018-04-03 08:47:40 +1000

    adding allowed hosts

--- oncallme/settings.py
+++ oncallme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oncallme.herokuapp.com']
 
 # Database environment variables
 PG_USER = os.environ.get('POSTGRES_USER')
