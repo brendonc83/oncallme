8e1ab79ff12def366e21d717213002eb0a803a0f
Brendon <bl.cleary@gmail.com>
2018-04-02 21:47:11 +1000

    collectstatic wars

--- oncallme/settings.py
+++ oncallme/settings.py
@@ -116,21 +116,17 @@ USE_TZ = True
 DATE_INPUT_FORMATS = ['%d/%m/%Y', '%Y/%m/%d']
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 LOGIN_REDIRECT_URL = '/index'
 LOGOUT_REDIRECT_URL = '/accounts/login'
